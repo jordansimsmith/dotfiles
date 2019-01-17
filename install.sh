c77b6854392b97ce3cfa0830b581a4062ec728f1
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-01-17 20:29:30 +1300

    Fixed vim-plug install command in install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Install vim-plug
-~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install vim-go
 git clone https://github.com/fatih/vim-go.git ~/.vim/plugged/vim-go
