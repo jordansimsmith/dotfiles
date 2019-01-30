880694876da9ef2d9b056ea50ab40591f8f1513d
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-01-31 06:08:52 +1300

    Added i3 config file

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Link dotfiles to home directory
-ln -sf $(pwd)vimrc ~/.vimrc
-ln -sf $(pwd)zshrc ~/.zshrc
-ln -sf $(pwd)Xresources ~/.Xresources
+ln -sf $(pwd)/vimrc ~/.vimrc
+ln -sf $(pwd)/zshrc ~/.zshrc
+ln -sf $(pwd)/Xresources ~/.Xresources
+ln -sf $(pwd)/i3config ~/.i3/config
