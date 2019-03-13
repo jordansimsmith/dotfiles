ba285d75e4f1e83092328ad61949d280fc434f23
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-03-13 17:35:31 +1300

    Alterd file structure to mimick home directory

--- link.sh
+++ link.sh
@@ -4,5 +4,5 @@
 ln -sf $(pwd)/vimrc ~/.vimrc
 ln -sf $(pwd)/zshrc ~/.zshrc
 ln -sf $(pwd)/Xresources ~/.Xresources
-ln -sf $(pwd)/i3config ~/.i3/config
+ln -sf $(pwd)/i3/config ~/.i3/config
 ln -sf $(pwd)/gitconfig ~/.gitconfig
