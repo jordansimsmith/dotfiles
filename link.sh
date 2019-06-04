c6746253e603c23751ecda5de96b5ad02fc24e31
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-06-05 10:19:13 +1200

    Added mimeapps.list (XDG)

--- link.sh
+++ link.sh
@@ -7,3 +7,4 @@ ln -sf $(pwd)/Xresources ~/.Xresources
 ln -sf $(pwd)/config/i3/config ~/.config/i3/config
 ln -sf $(pwd)/gitconfig ~/.gitconfig
 ln -sf $(pwd)/config/zathura/zathurarc ~/.config/zathura/zathurarc
+ln -sf $(pwd)/config/mimeapps.list ~/.config/mimeapps.list
