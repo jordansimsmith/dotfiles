14632309677cd1938d80158b50769bed12f30089
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-06-06 02:42:05 +1200

    Added to installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install yay
+sudo pacman -S yay
+
 # Install zsh
 sudo pacman -S zsh
 
@@ -13,3 +16,30 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install golang
+sudo pacman -S go
+
+# Install node.js 
+sudo pacman -S nodejs npm yarn
+
+# Install python tools
+sudo pip install autopep8 flake8
+
+# Install latex
+sudo pacman -S texlive-most
+
+# Install zathura
+sudo pacman -S zathura
+
+# Install vifm
+sudo pacman -S vifm
+
+# Install docker
+sudo pacman -S docker docker-compose
+
+# Install chrome
+sudo pacman -S chromium
+
+# Install other utilities
+sudo pacman -S zip unzip xclip ffmpeg cloc
